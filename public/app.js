93098669993ba571028955fe551d4837286f2d93
halemo91 <mo7amed.7alem_2012@hotmail.com>
2018-10-24 22:45:04 +0200

    get subtasks

--- public/app.js
+++ public/app.js
@@ -1,33 +1,47 @@
 
 var app1 = angular.module('consumeRestApp', ['ngResource','ui.grid', 'ui.grid.edit']);
 
-// app1.constant("baseURL", "http://localhost:3000/");
+ app1.constant("baseURL", "http://localhost:3000/");
 app1.factory("tasks", function($resource) {
     return $resource("http://localhost:3000/tasks");
 });
-// app1.service('feedbackService',['$resource','baseURL',function($resource,baseURL){
-//       this.getFeedback=function(){
-//         return $resource(baseURL+"tasks/id",null,{
-//           'update':{
-//             method:'PUT'
-//           }
-//         });
-//       };
-//     }]);
+app1.service('getSubTasks',['$resource','baseURL',function($resource,baseURL){
+      this.getFeedback=function(id){
+        return $resource(baseURL+"tasks/"+id+"/subtasks",null,{
+          'update':{
+            method:'GET'
+          }
+        });
+      };
+    }]);
 
-app1.controller('ctrl1', ['$scope','tasks', function($scope,tasks) {
+app1.controller('ctrl1', ['$scope','tasks','getSubTasks', function($scope,tasks,getSubTasks) {
 $scope.test = [];
   tasks.query(function(data) {
      $scope.tasks = data;
-     $scope.gridOptions.data = data;
+     $scope.tasks.forEach(function(obj) {
+       obj.subTask = [];
 
-     $scope.test.push(data);
+         $scope.subTasksService = getSubTasks.getFeedback(obj.id).query(function(response) {
+            response.forEach(function(res) {
+                    if(res.taskId == obj.id){
+                      obj.subTask.push(res);
+
+                    }
+            });
+         });
+
+
+      });
+
+     $scope.gridOptions.data = $scope.tasks;
+     console.log('griddd',$scope.tasks)
+
  }, function(err) {
      console.error("Error occured: ", err);
  });
     $scope.first = 1;
      $scope.second = 1;
-     console.log('ddd',$scope.test )
      $scope.gridOptions = {
         columnDefs: [
           { field: 'name' },
@@ -36,13 +50,11 @@ $scope.test = [];
         { field: 'category' }
         ]
       };
-        // $scope.feedbacks = feedbackService.getFeedback().query(function(response) {
-        //     $scope.feedbacks = response;
-        // });
+
         // $scope.sendFeedback = function() {
         //         $scope.feedback.date = new Date().toISOString();
         //         $scope.feedbacks.push($scope.feedback);
-        //         feedbackService.getFeedback().update($scope.feedbacks);
+        //         getSubTasks.getFeedback().update($scope.feedbacks);
         //         $scope.feedbackForm.$setPristine();
         //         $scope.feedback = {firstName: "",lastName: "",email: "", date:""};
         // };
